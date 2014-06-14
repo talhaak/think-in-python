0c423d0983ef2f5d9e8604e00ca95bea63b39537
Talha Akyildiz <talhaa@outlook.com>
2014-06-14 19:49:43 +0300

    Chapter 1 and 2 are implemented
    - I learned variables, types
    - I learned Python has two different numbers integer, float.
    - I learned script and interactive modes and their differences.
    - I learned type() function
    - I learned string concatenation

---
+++ chapter1/chapters1and2.py
@@ -0,0 +1,102 @@
+'''
+Created on 12 Haz 2014
+
+@author: Belief
+'''
+#Print statement 
+from __future__ import division
+
+import math
+
+print 'talha'
+
+print 7
+
+print type(3)
+
+print type(3.111)
+
+print type('x')
+
+print type(1000000)
+
+print 1,000,000
+
+print 3
+
+#Variables 
+
+message = 'You need to improve yourself'
+
+print message 
+
+number = 5658879
+
+print number 
+
+pi = 3.14
+
+print pi
+
+print 01
+
+print 010
+
+print 0100
+
+print 01000
+
+print 02132
+
+#Operations
+
+print 5 / 2
+
+print 5 // 2
+
+print 5.0 / 2
+
+print 5 / 2.0
+
+# order in operations 
+
+print 5+3/2
+
+print (5+3) / 2 
+
+# String operations
+
+name = 'talha' 
+
+surname = 'akyildiz'
+
+print name +  ' ' + surname
+
+print name * 2  
+
+# Exercises 
+
+pi = 3.14
+
+r = 5
+
+volume = (4/3) * pi * (r**3)
+
+print volume 
+
+cover_price = 24.95 
+
+discount = 0.4
+
+first_cost = 3 
+
+second_cost = 0.75
+
+number = 60
+
+total_cost = (cover_price*(1-discount)-first_cost) + (number-1) * ((cover_price)*(1-discount)-second_cost) 
+
+print total_cost
+
+
+
