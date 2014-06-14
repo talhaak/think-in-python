7b7ab92c220f0452b5273486fca54e400989bff3
Talha Akyildiz <talhaa@outlook.com>
2014-06-14 20:02:22 +0300

    Chapter 3 is implemented
    - I learned to implement void function.
    - I learned to implement sum_ function by taking two integer parameter.
    - I learned to use math class functions.

---
+++ chapter3/functions.py
@@ -0,0 +1,39 @@
+'''
+Created on 13 Haz 2014
+
+@author: Belief
+'''
+
+import math
+
+#If I keep on believing the dream that I wish will come true.
+
+# Type conversion functions
+
+print int( 3.5)
+
+def say_name():
+    
+    print "I'm Talha"
+    
+    print 'My university is Bilkent'
+    
+say_name()    
+
+def sum_(n1,n2):
+    return n1+n2
+
+print sum_(3,5)
+
+x = math.pow(2, 5)
+
+print x
+
+y = math.factorial(5)
+
+print y
+
+z = math.log(math.exp(2))
+             
+print z              
+
